1f3573e7b3088406f851e255954e4e33b24a61d7
Miha Jan Strehovec <48392708+mihajanstrehovec@users.noreply.github.com>
2021-05-24 10:09:51 +0200

    Update Ping-OCR.py

--- Ping-OCR.py
+++ Ping-OCR.py
@@ -7,10 +7,12 @@ from datetime import datetime, timedelta
 import glob
 import numpy as np
 import re
-import pandas as pd
 import plotly as px
 import time
+import os
 
+
+__dir__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 class GUI(tk.Tk):
     def __init__(self):
@@ -56,9 +58,7 @@ class GUI(tk.Tk):
     
     def make_screenshots(self, bboxNum):
         
-        
-
-        time.sleep(10)
+        time.sleep(5)
 
         # Setting start time and end time, so while loop for screenshot taking ends after (duration) minutes
         start = datetime.now()
@@ -72,11 +72,12 @@ class GUI(tk.Tk):
         while datetime.now() < end:
             im = ImageGrab.grab(bbox = (bboxNum)) # bbox = (window placement and size)
             dt = datetime.now()
-            fname = "C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images/pic_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000)
+            fname = os.path.join(__dir__, "images/screenshot_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000))
+            #fname = "C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images/pic_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000)
             im.save(fname, 'png')
             time.sleep(0.4) # taking a screenshot every 0.5s
         
-
+        print("Screenshot area coordinates", bboxNum)
         root.quit()   
 
 print("\n")
@@ -85,7 +86,7 @@ print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 input("Press enter to start the screenshot sellection area - I recommend you to already have the game of choice open and ping displayed at this point")
 
-time.sleep(10) # Wait for 10 seconds so you can open your game
+time.sleep(5) # Wait for 10 seconds so you can open your game
 
 root = GUI()
 
@@ -96,7 +97,8 @@ print("done Screenshoting")
 def resize_images():
     
     # Opening all screenshots
-    images=glob.glob("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images/*.png")
+    #images=glob.glob("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images/*.png")
+    images=glob.glob(os.path.join(__dir__, "images/*.png"))
 
     for image in images:
         set_image_dpi(image)
@@ -105,20 +107,28 @@ def set_image_dpi(img):
 
     # Setting the dpi of the images to 300 and saving them to a new folder
     im = Image.open(img)
+    print(img)
+    
+
     length_x, width_y = im.size
     factor = min(1, float(1024.0 / length_x))
     size = int(factor * length_x), int(factor * width_y)
     im_resized = im.resize(size, Image.ANTIALIAS)
-    im_resized.save("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images_dpi/{}".format(img.split('\\')[1]), dpi=(300, 300))
+    #im_resized.save("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images_dpi/{}".format(img.split('\\')[1]), dpi=(300, 300))
+    print(os.path.join(__dir__, "images_dpi/{}".format(img.split('\\')[1])))
+    im_resized.save(os.path.join(__dir__, "images_dpi/{}".format(img.split('\\')[7])), dpi=(300, 300))
 
 def OCR_ping_read():
     
-    # Opeinng all screenshots
-    images=glob.glob("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images_dpi/*.png")
+    # Opening all screenshots
+    #images=glob.glob("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images_dpi/*.png")
+    images=glob.glob(os.path.join(__dir__, "images_dpi/*.png"))
 
     custom_config = r'--oem 3 --psm 6'
 
     pingArr = []
+    allPings = 0
+    failedPing = 0
 
     # OCR loop
     for image in images:
@@ -127,50 +137,62 @@ def OCR_ping_read():
         
         # Editing the image so the OCR is more accurate 
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.medianBlur(img,1) # noise removal 
+        #img = cv2.medianBlur(img,1) # noise removal 
         img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] # 0 and 1 image (black or white)
         
 
-        print(pytesseract.image_to_string(img, config=custom_config))
+        #print(pytesseract.image_to_string(img, config=custom_config))
         pingArr.append(pytesseract.image_to_string(img, config=custom_config)) # the OCR algorithm tesseract
 
         
     latency = []
 
     for ping in pingArr:
+        allPings += 1
         
         # Extracting just the number from the string | e.g. ping: 24 ms - 24
-        if (not re.findall(r'\b\d+\b', ping)) == False: # If there is no number in the string simply skip that measurment
-                latency.append(re.findall(r'\b\d+\b', ping)[0]) #
+        if (ping.find("ping") >= 0): # If there is no number in the string simply skip that measurment
+                ind = ping.find("ping")
+                hodl = ping[ind:]
+                if(len(re.findall(r'\b\d+\b', hodl)) > 0):
+                    latency.append(re.findall(r'\b\d+\b', hodl)[0])
+                else:
+                    failedPing += 1 
+        else:
+            failedPing +=1
 
-    return latency
+    return latency, allPings, failedPing
 
     
 
 def OCR():
     
     t0 = time.time()
-   
-    # You can implement this if you don't want to add tesseract install folder to path / something isn't working
+    
+    #########
+    # You can use this code if you don't want to add tesseract install folder to path / something isn't working
     # accessing tesseract executable -> change to your destination!!
     # pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe' 
+    #########
 
     input("Press enter to start the OCR")
 
     resize_images()
 
-    latency = OCR_ping_read()
+    latency, aPing, fPing = OCR_ping_read()
 
-    timeOfMeasurment = str(datetime.now())
+    timeOfMeasurment = str(datetime.now().strftime("%H%M_%S"))
     timeOfMeasurment = timeOfMeasurment.split(" ")[0]
    
 
-    np.savetxt("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/ping_data/ping-data-{}.csv.".format(timeOfMeasurment), # Saving the CSV file 
+    np.savetxt(os.path.join(__dir__,"ping_data/ping-data-{}.csv.".format(timeOfMeasurment)), # Saving the CSV file 
             latency,
             delimiter =", ", 
             fmt ='% s')
 
-    data = pd.read_csv("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/ping_data/ping-data-{}.csv".format(timeOfMeasurment)) # Reading the CSV file
+    data = np.genfromtxt(os.path.join(__dir__, "ping_data/ping-data-{}.csv".format(timeOfMeasurment)), delimiter = ",") # Reading the CSV file
+
+    
 
     fig = px.plot(data, title='CSGO - Ping', kind="line") # Plotting the graph
     
