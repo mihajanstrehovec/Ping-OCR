e3dc98963318863166d839edae0ae1741f4f2adb
Miha Jan Strehovec <48392708+mihajanstrehovec@users.noreply.github.com>
2021-08-09 12:32:35 +0200

    Update Ping-OCR.py

--- Ping-OCR.py
+++ Ping-OCR.py
@@ -14,8 +14,18 @@ import time
 import os
 import pyscreenshot
 import platform
+from pathlib import Path
+
+
+# CSGO - 1354 875 1570 1003
+# FORTNITE - 421 2 500 44
 
 __dir__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+program_start = datetime.now().strftime("%d%m%y%H%M")
+
+Path(os.path.join(__dir__, "images")).mkdir(parents = True, exist_ok = True) 
+Path(os.path.join(__dir__, "images_dpi")).mkdir(parents = True, exist_ok = True) 
+Path(os.path.join(__dir__, "ping_data")).mkdir(parents = True, exist_ok = True) 
 
 
 def make_screenshots(bboxNum):
@@ -31,12 +41,13 @@ def make_screenshots(bboxNum):
         #duration = 0.1 # in minutes
 
         end = start + timedelta(minutes = float(duration))
+        Path(os.path.join(__dir__, "images", program_start)).mkdir(parents = True, exist_ok = True) 
 
         # Taking screenshots and saving them
         while datetime.now() < end:
             im = pyscreenshot.grab(bbox = (bboxNum)) # bbox = (window placement and size)
             dt = datetime.now()
-            fname = os.path.join(__dir__, "images", "screenshot_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000))
+            fname = os.path.join(__dir__, "images",program_start, "screenshot_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000))
             #fname = "C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images/pic_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000)
             im.save(fname, 'png')
             time.sleep(0.4) # taking a screenshot every 0.5s
@@ -101,15 +112,18 @@ class GUI(tk.Tk):
         ## How long the script takes screenshots
         #duration = 0.1 # in minutes
 
-        end = start + timedelta(minutes = float(duration))
+        Path(os.path.join(__dir__, "images", program_start)).mkdir(parents = True, exist_ok = True) 
 
+        end = start + timedelta(minutes = float(duration))
+        screenshot_number = 0
         # Taking screenshots and saving them
         while datetime.now() < end:
             im = pyscreenshot.grab(bbox = (bboxNum)) # bbox = (window placement and size)
             dt = datetime.now()
-            fname = os.path.join(__dir__, "images", "screenshot_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000))
+            fname = os.path.join(__dir__, "images",program_start, "screenshot_{}__{}.png".format(dt.strftime("%H%M_%S"), screenshot_number))
             #fname = "C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images/pic_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000)
             im.save(fname, 'png')
+            screenshot_number += 1
             time.sleep(0.4) # taking a screenshot every 0.5s
         
         print("Screenshot area coordinates", bboxNum)
@@ -135,10 +149,12 @@ if(inputType == "S"):
 
 elif (inputType == "K"):
     bbox = [0,0,0,0]
-    bbox[0] = input("Enter start of width: ")
-    bbox[1] = input("Enter start of height:")
-    bbox[2] = input("Enter end of width:")
-    bbox[3] = input("Enter end of height:")
+    bbox[0] = int(input("Enter start of width: "))
+    bbox[1] = int(input("Enter start of height:"))
+    bbox[2] = int(input("Enter end of width:"))
+    bbox[3] = int(input("Enter end of height:"))
+
+    #print(bbox)
 
     make_screenshots(bbox)
 
@@ -152,7 +168,7 @@ def resize_images():
     
     # Opening all screenshots
     #mages=glob.glob("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images/*.png")
-    images=glob.glob(os.path.join(__dir__, "images", "*.png"))
+    images=glob.glob(os.path.join(__dir__, "images", program_start, "*.png"))
 
     for image in images:
         set_image_dpi(image)
@@ -170,13 +186,13 @@ def set_image_dpi(img):
     im_resized = im.resize(size, Image.ANTIALIAS)
     #im_resized.save("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images_dpi/{}".format(img.split('\\')[1]), dpi=(300, 300))
   
-    im_resized.save(os.path.join(__dir__, "images_dpi", "s{}".format(img.lstrip(os.path.join(__dir__ , "Ping-OCR", "images")))), dpi=(300, 300))
+    im_resized.save(os.path.join(__dir__, "images_dpi",program_start, "s{}".format(img.lstrip(os.path.join(__dir__ , "Ping-OCR", "images", program_start)))), dpi=(300, 300))
 
 def OCR_ping_read():
     
     # Opening all screenshots
     #images=glob.glob("C:/Users/Miha_Plume/Desktop/Plume/Ping-OCR/images_dpi/*.png")
-    images=glob.glob(os.path.join(__dir__, "images_dpi", "*.png"))
+    images=glob.glob(os.path.join(__dir__, "images_dpi",program_start, "*.png"))
 
     custom_config = r'--oem 3 --psm 6'
 
@@ -186,44 +202,72 @@ def OCR_ping_read():
     pingArr = []
     allPings = 0
     failedPing = 0
+    failed = 0
 
     # OCR loop
     for image in images:
+        allPings += 1
         
         img = cv2.imread(image)
+
+        imgOCR = Image.fromarray(img)
         
         # Editing the image so the OCR is more accurate 
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #img = cv2.medianBlur(img,1) # noise removal 
-        img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] # 0 and 1 image (black or white)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # img = cv2.medianBlur(img,1) # noise removal 
+        # img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] # 0 and 1 image (black or white)
 
-        img = Image.fromarray(img)
-        
+        txt_no_edit = tool.image_to_string(imgOCR, lang = "eng", builder = pyocr.builders.TextBuilder())
 
-        #print(pytesseract.image_to_string(img, config=custom_config))
-        #pingArr.append(pytesseract.image_to_string(img, config=custom_config)) # the OCR algorithm tesseract
-        txt = tool.image_to_string(img, lang = "eng", builder = pyocr.builders.TextBuilder())
-        print(txt) # You can print this to check OCR
-        pingArr.append(txt)
+        if (len([int(s) for s in txt_no_edit.split() if s.isdigit()]) > 0): # If there is no number in the string simply skip that measurment
+                pingArr.append([int(s) for s in txt_no_edit.split() if s.isdigit()][0])
+        else:
+            kernel = np.ones((2,2),np.uint8)
+            img =  cv2.dilate(img, kernel, iterations = 1)
+            kernel = np.ones((1,1),np.uint8)
+            img = cv2.erode(img,kernel,iterations = 3)
+            img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.threshold(img,0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] # 0 and 1 image (black or white)
+            cv2.Canny(img, 100, 200)
+            imgOCR = Image.fromarray(img)
+
+            txt_edit = tool.image_to_string(imgOCR, lang = "eng", builder = pyocr.builders.TextBuilder())
+            
+            if (len([int(s) for s in txt_edit.split() if s.isdigit()]) > 0): # If there is no number in the string simply skip that measurment
+                pingArr.append([int(s) for s in txt_edit.split() if s.isdigit()][0])
+
+            else:
+                img = cv2.bitwise_not(img)
+                imgOCR = Image.fromarray(img)
+
+                txt_edit_inverse = tool.image_to_string(imgOCR, lang = "eng", builder = pyocr.builders.TextBuilder())
+
+                if (len([int(s) for s in txt_edit_inverse.split() if s.isdigit()]) > 0): # If there is no number in the string simply skip that measurment
+                    pingArr.append([int(s) for s in txt_edit_inverse.split() if s.isdigit()][0])
+
+                else:
+                    failed += 1
+
 
         
     latency = []
+    #failed = {}
 
-    for ping in pingArr:
-        allPings += 1
+    return pingArr, allPings, failed
+
+    # for ping in pingArr:
+    #     allPings += 1
         
-        # Extracting just the number from the string | e.g. ping: 24 ms - 24
-        if (ping.find("ping") >= 0): # If there is no number in the string simply skip that measurment
-                ind = ping.find("ping")
-                hodl = ping[ind:]
-                if(len(re.findall(r'\b\d+\b', hodl)) > 0):
-                    latency.append(re.findall(r'\b\d+\b', hodl)[0])
-                else:
-                    failedPing += 1 
-        else:
-            failedPing +=1
+    #     # Extracting just the number from the string | e.g. ping: 24 ms - 24
+    #     if (len([int(s) for s in ping.split() if s.isdigit()]) > 0): # If there is no number in the string simply skip that measurment
+    #             latency.append([int(s) for s in ping.split() if s.isdigit()][0])
+                    
+    #     else:
+    #         failedPing +=1
+    #         failed[allPings] = ping
 
-    return latency, allPings, failedPing
+    # return latency, allPings, failedPing, failed
 
     
 
@@ -239,16 +283,24 @@ def OCR():
 
     input("Press enter to start the OCR")
 
+    Path(os.path.join(__dir__, "images_dpi", program_start)).mkdir(parents = True, exist_ok = True) 
+
     resize_images()
 
-    latency, aPing, fPing = OCR_ping_read()
+    latency, aPing, failed = OCR_ping_read()
 
     print("All ping: ", aPing)
-    print("Failed pings: ", fPing)
+    # print("Failed pings: ", fPing)
+    print("These are all the failed pings with their corresponding OCR string:", failed)
 
     if(len(latency) < 1):
         print("Something went wrong during OCR, please try to optimize the screenshots (placement, backrgound...)")
         exit()
+
+    for i  in range(len(latency)):
+        if i < len(latency)-10:
+            if latency[i] > latency[i-1]*10 and latency[i] > latency[i+1]*10 and latency[i] > latency[i+2]*10 and latency[i] > latency[i+3]*10:
+                latency.pop(i)   
 
     timeOfMeasurment = str(datetime.now().strftime("%H%M_%S"))
     timeOfMeasurment = timeOfMeasurment.split(" ")[0]
